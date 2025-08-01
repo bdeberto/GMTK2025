bfeaaf910f2b79b99d9cb5a64290f1bc91927a03
bdeberto <bad0719@gmail.com>
2025-08-01 12:16:16 -0400

    oh my god ARMS

--- Assets/Game/Scripts/LimbConnect.cs
+++ Assets/Game/Scripts/LimbConnect.cs
@@ -6,10 +6,13 @@ public class LimbConnect : MonoBehaviour
     public Transform Root = default;
     public Transform Target = default;
 
+    LineRenderer lr = default;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = Connectors.Length + 2;
     }
 
     // Update is called once per frame
@@ -19,10 +22,13 @@ public class LimbConnect : MonoBehaviour
         float yDist = Target.position.y - Root.position.y;
         float xStep = xDist / Connectors.Length;
         float yStep = yDist / Connectors.Length;
+        lr.SetPosition(0, Root.position);
         for (int i = 0; i < Connectors.Length; ++i)
         {
             Vector3 connectorPos = Root.position + new Vector3(xStep * i, yStep * i);
-            Connectors[i].position = connectorPos;
-        }
+            Connectors[i].position = Vector3.Lerp(Connectors[i].position, connectorPos, Time.deltaTime * 3f * (i + 1));
+            lr.SetPosition(i + 1, Connectors[i].position);
+		}
+		lr.SetPosition(lr.positionCount - 1, Target.position);
     }
 }
